29daaab2ace9f00e3d15f4349231a1baca13db44
russellkt <russellkt@gmail.com>
2026-01-22 10:29:07 -0600

    Initial Commit

---
+++ RaceCompass/RaceCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaceCompassApp.swift
+//  RaceCompass
+//
+//  Created by russellkt on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct RaceCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
