9070c874bf14703396b6c2b5af1d396b9814afbb
russellkt <russellkt@gmail.com>
2026-01-22 10:29:07 -0600

    Initial Commit

---
+++ RaceCompass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RaceCompass
+//
+//  Created by russellkt on 1/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
